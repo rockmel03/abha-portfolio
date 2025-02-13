13a1375446efa87d0ff26f8fe2d0ea5106238e98
rockmel03 <kamalswag2@gmail.com>
2025-02-13 19:33:33 +0530

    Refactor about section layout and enhance skills display; update marquee styling and implement tools showcase

--- src/script.js
+++ src/script.js
@@ -51,53 +51,12 @@ function initMarquee() {
   });
 }
 
-initSmoothScrolling();
-function initSmoothScrolling() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("main"),
-    smooth: true,
-  });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  locoScroll.on("scroll", ScrollTrigger.update);
-
-  // tell ScrollTrigger to use these proxy methods for the "main" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("main").style.transform
-      ? "transform"
-      : "fixed",
-  });
-
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-}
-
 initGallaryAnime();
 function initGallaryAnime() {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: "#gallary",
-      scroller: "main",
+      scroller: "body",
       start: "50% 80%",
       end: "50%, 20%",
       //   markers: true,
@@ -208,9 +167,55 @@ function initParaTypoEffect() {
     stagger: 0.002,
     scrollTrigger: {
       trigger: ".typo ",
-      scroller: "main",
+      scroller: "body",
       start: "50% 90%",
       end: "50%, 50%",
     },
+  });
+}
+
+displayTools();
+function displayTools() {
+  const tools = [
+    {
+      name: "illustrator",
+      imgURL: "./assets/images/adobe-illustrator.png",
+    },
+    {
+      name: "procreate",
+      imgURL: "./assets/images/procreate.png",
+    },
+    {
+      name: "figma",
+      imgURL: "./assets/images/figma.png",
+    },
+    {
+      name: "photoshop",
+      imgURL: "./assets/images/adobe-photoshop.png",
+    },
+    {
+      name: "autocad  ",
+      imgURL: "./assets/images/autocad.png",
+    },
+  ];
+
+  let toolsContainer = document.querySelector("#tools-container");
+
+  tools.forEach((tool) => {
+    const toolItem = document.createElement("div");
+    toolItem.innerHTML = `
+          <div class="flex gap-1 items-center">
+            <div class="w-14 bg-gray-900 p-2   aspect-square rounded-xl ">
+              <img
+                src="${tool.imgURL}"
+                alt=""
+                class="w-full h-full object-cover"
+              />
+            </div>
+           
+          </div>
+`;
+
+    toolsContainer.appendChild(toolItem);
   });
 }
